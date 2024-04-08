96bad793a000af64bf7bf500e81ccc6b1f139ded
Johannes Voigt (Finatix) <j.voigt@finatix.de>
2024-04-08 19:40:01 +0200

    add embedded data as mdx

---
+++ src/content/addresses/sfr.md
@@ -0,0 +1,4 @@
+---
+title: Sächsischer Flüchtlingsrat
+url: https://www.saechsischer-fluechtlingsrat.de/de/projekte/asylberatung/
+---
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,7 @@
-import { z, defineCollection,reference } from "astro:content";
+import {defineCollection, z, reference} from "astro:content";
 
 const flyersCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
+  type: 'content', schema: z.object({
     title: z.string(),
     blocks: z.array(reference('blocks')
     )
@@ -10,13 +9,18 @@ const flyersCollection = defineCollection({
 });
 
 const blocksCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
+  type: 'content', schema: z.object({
     title: z.string()
   })
 });
 
+const addressesCollection = defineCollection({
+  type: 'content', schema: z.object({
+    title: z.string(),
+    url: z.string()
+  })
+});
+
 export const collections = {
-  flyers: flyersCollection,
-  blocks: blocksCollection
+  flyers: flyersCollection, blocks: blocksCollection, addresses: addressesCollection
 };
